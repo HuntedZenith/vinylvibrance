b98477080e7317e052032749d35d3e0b46475878
HuntedZenith <syler.shurtliff@gmail.com>
2026-07-25 10:59:56 -0600

    Initial Fabric Project

---
+++ src/client/java/io/github/huntedzenith/vinylvibrance/client/VinylVibranceClient.java
@@ -0,0 +1,10 @@
+package io.github.huntedzenith.vinylvibrance.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VinylVibranceClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
